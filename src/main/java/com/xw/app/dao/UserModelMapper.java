71e0c71f48c258649b9c9f68829b9a1251c2efff
Wangj@ENVY STS <656876071@qq.com>
2018-12-07 21:53:54 +0800

    Default Changelist

---
+++ src/main/java/com/xw/app/dao/UserModelMapper.java
@@ -0,0 +1,17 @@
+package com.xw.app.dao;
+
+import com.xw.app.model.UserModel;
+
+public interface UserModelMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(UserModel record);
+
+    int insertSelective(UserModel record);
+
+    UserModel selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(UserModel record);
+
+    int updateByPrimaryKey(UserModel record);
+}
